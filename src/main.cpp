f346a2174d5896b47ef7567209c23febc55f7faa
Sergio Aguado <sergio.aguado@cuatroochenta.com>
2019-10-26 14:19:02 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN   5     // Configurable, see typical pin layout above
+#define SS_PIN    53   // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+
+MFRC522::MIFARE_Key key;
+
+void setup() {
+  Serial.begin(9600);  // Initialize serial communications with the PC
+  while (!Serial);     // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();         // Init SPI bus
+  mfrc522.PCD_Init();  // Init MFRC522 card
+
+}
+
+void loop() {
+  
+  // Look for new cards, and select one if present
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+    delay(50);
+    return;
+  }
+  
+  // Now a card is selected. The UID and SAK is in mfrc522.uid.
+  
+  // Dump UID
+  String content = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  } 
+  Serial.print(F("Card UID:"));
+  Serial.println(content);
+
+  content.toUpperCase();
+  if (content.substring(1) == "D8 D6 BD 49"){
+    Serial.print("Abrir");
+  }
+  else if (content.substring(1) == "20 36 7A A2"){
+    Serial.print("No abrir");
+  }
+  else{
+    Serial.print("Tarjeta desconocida");
+  }
+
+  
+  delay(500);
+}
